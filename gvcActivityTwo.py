d903cb482eb81cc8bb830f7889e11d5d841be46c
Rhunnan <rhunnandwight.liao.21@usjr.edu.ph>
2024-03-10 21:01:08 +0800

    added sobel,laplacian, and scharr edge detection of an iamge

--- gvcActivityTwo.py
+++ gvcActivityTwo.py
@@ -1,32 +1,44 @@
 import cv2
 import numpy as np
+#activity 2
 
 def getPixelValue(x, y, image):
     #getting the pixel value using the [x, y ] method, ang pixel nga gekuha nako ani kay coordinate x og y na value
     pixelVal = image[x,y]
 
     return pixelVal
-#activity 1
-eagleFile = "pheagle.jpg"
+
+lionImg = "lionAbstract.webp"
 
 #grayscaled imgae para gamiton sa thresholding
-img2 = cv2.imread(eagleFile, cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread(lionImg, cv2.IMREAD_GRAYSCALE)
 # ako gekuha ang dimension sa image ron makuha nako ang size or ang height og width sa image 
 dimensions = img2.shape
 print("shape of the image load ", img2.shape)
 #converting image to black and white by thresholding the img2 which is already in grayscaled
 ret, blackWhiteImg = cv2.threshold(img2, 127, 255, cv2.THRESH_BINARY)
 
+#finding laplacian edges using laplacian method in cv2
+laplacianEdges = cv2.Laplacian(img2,cv2.CV_64F)
 
+#finding Sobel edges of an image
+sobelx = cv2.Sobel(img2, cv2.CV_64F, 1,0)
+sobely = cv2.Sobel(img2, cv2.CV_64F, 0,1)
+sobelEdges = cv2.bitwise_or(sobelx, sobely)
+
+#finding scharr edges of an image 
+scharrx = cv2.Scharr(img2, cv2.CV_64F, 1, 0)
+scharry = cv2.Scharr(img2, cv2.CV_64F, 0, 1)
+scharr_edges = cv2.bitwise_or(scharrx, scharry)
 
 while True:
         
     print("MENU: Select flag to display image:")
     print("input [1] to get the size of an Image")
     print("input [2] to get BGR pixel of an image")
-    print("input [3] for Unchange Image flag")
-    print("input [4] for Black and White Image")
-    print("input [5] to get BGR values of a pixel")
+    print("input [3] to display laplacian Edges of the image")
+    print("input [4] to display sobel Edges of the image")
+    print("input [5] to display")
 
     print("input [0] to Exit")
 
@@ -34,17 +46,18 @@ while True:
     if selected == '0':
         break
     elif selected == "1":
-        print("Image heigt: {} Image width: {}".format(dimensions[0], dimensions[1]))
+        print(f"Image heigt: {dimensions[0]} Image width: {dimensions[1]}")
     elif selected == "2":
         x = int(input("Input Coordinate X: "))
         y = int(input("Input Coordinate y: "))
         
         pixelVal = getPixelValue(x,y, blackWhiteImg)
-        #since colored manining img1 kay gamit og flag na cv2.IMREAD_COLOR so naa ni 3 ka values which is ang BGR
+        # since ang image na ge kwaan nakog value is black and white then it would have only one value dli same sa colored na naar BGR values
         print(f"Pixel Value at {x}, {y} = {pixelVal}")
-    # elif selected == "3":
-    # elif selected == "4":
-    #     cv2.imshow('ealgeBW', blackWhiteImg)
+    elif selected == "3":
+        cv2.imshow('laplacianEdge', laplacianEdges)
+    elif selected == "4":
+        cv2.imshow('sobelEdges', sobelEdges)
     # elif selected == "5":
         
     else:
